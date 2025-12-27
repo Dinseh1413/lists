8c8a79648e34d0850fb84d46ce972ed34794ee80
Dinseh1413 <126776105+Dinseh1413@users.noreply.github.com>
2025-12-27 11:39:44 +0530

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,6 @@
+// supabase.js
+const SUPABASE_URL = 'https://xoqtcvydgbtskmqdjdog.supabase.co';
+const SUPABASE_KEY = 'sb_publishable_xIyOekuY0ZTw_n-IfdJpzg_042n4v-W';
+
+// Make client available globally
+const sbClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
